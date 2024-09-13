ec94bd80e70da96781a56ffe055d2d972d80299b
Rayytra <rayytra@gmail.com>
2024-09-13 11:09:36 +0700

    added Navbar and styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKat</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Navbar from './Navbar.svelte';
+</script>
+
+<Navbar />
+<body>
+	<h1 class="">Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</body>
+
+<style>
+	body {
+		margin: 0;
+		background-image: linear-gradient(to top, #000000 40%, rgba(0, 0, 0, 0)), url(./incofurbg.png);
+		background-repeat: no-repeat;
+		background-size: cover;
+		background-attachment: fixed;
+	}
+</style>
---
+++ src/routes/Navbar.svelte
@@ -0,0 +1,17 @@
+<header>
+	<h1 class="coolvetica" style="font-weight: 100;">Incofur</h1>
+	<nav></nav>
+</header>
+
+<style>
+    h1 {
+        height: fit-content;
+    }
+    header{
+        display: flex;
+        height: fit-content;
+        background-color: darkslategray;
+        border-radius: 10px;
+        padding-inline: 1em;
+    }
+</style>
